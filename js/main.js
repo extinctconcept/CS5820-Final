35e7529d1dff302eca16948ab59d8acea0934d86
Matthew Kidd <mattofthekidd@gmail.com>
2020-11-16 21:34:58 -0700

    data changes
    Added lines for processing our data files.

--- js/main.js
+++ js/main.js
@@ -9,13 +9,143 @@ class Main {
         this.timeline = new Timeline(this.classSelector, this.info, this.line);
     }
 
+    //Our terrorist data needs to have this added.
+    // Only included states that were present in data so no Alaska, Maine, Montana, Rhode Island, South Dakota, Vermont, or Wyoming.
+    whatIsStateCode(state) {
+        switch(state) {
+            case "Alabama":
+                return "AL";
+            case "Arizona":
+                return "AZ";
+            case "Arkansas":
+                return "AR";
+            case "California":
+                return "CA";
+            case "Colorado":
+                return "CO";
+            case "Connecticut":
+                return "CT";
+            case "District of Columbia":
+                return "DC";
+            case "Florida":
+                return "FL";
+            case "Georgia":
+                return "GA";
+            case "Idaho":
+                return "ID";
+            case "Illinois":
+                return "IL";
+            case "Iowa":
+                return "IA";
+            case "Kansas":
+                return "KS";
+            case "Kentucky":
+                return "KY";
+            case "Louisiana":
+                return "LA";
+            case "Maryland":
+                return "MD";
+            case "Massachusetts":
+                return "MA";
+            case "Michigan":
+                return "MI";
+            case "Minnesota":
+                return "MN";
+            case "Mississippi":
+                return "MS";
+            case "Missouri":
+                return "MO";
+            case "Nebraska":
+                return "NE";
+            case "Nevada":
+                return "NV";
+            case "New Hampshire":
+                return "NH";
+            case "New Jersey":
+                return "NJ";
+            case "New Mexico":
+                return "NM";
+            case "New York":
+                return "NY";
+            case "North Carolina":
+                return "NC";
+            case "North Dakota":
+                return "ND";
+            case "Ohio":
+                return "OH";
+            case "Oklahoma":
+                return "OK";
+            case "Oregon":
+                return "OR";
+            case "Pennsylvania":
+                return "PA";
+            case "Puerto Rico":
+                return "PR";
+            case "South Carolina":
+                return "SC";
+            case "Tennessee":
+                return "TN";
+            case "Texas":
+                return "TX";
+            case "U.S. Virgin Islands":
+                return "VI";
+            case "Utah":
+                return "UT";
+            case "Virginia":
+                return "VA";
+            case "Washington":
+                return "WA";
+            case "West Virginia":
+                return "WV";
+            case "Wisconsin":
+                return "WI";
+            default:
+                //two items may return null
+                return null;
+        }
+    }
+
     reRender(year) {
-        d3.csv("assets/beer.csv")
-        .then((data) => {   
-            this.cartogram.render(data, year)
-            this.timeline.render(data, year);
-            this.bar.render(data, year);
-        });
+        //Natural Disaters
+        //from FEMA
+        d3.csv("data/DisasterDeclarationsSummaries.csv")
+        .then((data) => {
+            
+        })
+        //Terrorism data
+        //trimmed from source to have only US data from 2000-2017
+        d3.csv("data/globalterrorismdb_0718dist.csv", d => {
+            d.stateCode = this.whatIsStateCode(d.provstate);
+            
+        })
+        .then((data) => {
+            
+        })
+        //Stock data
+        d3.csv("data/SPY_Historical_Data.csv")
+        .then((data) => {
+            
+        })
+        //Flight data
+        d3.csv("data/USCarrier_Traffic_20201106204344.csv")
+        .then((data) => {
+            
+        })
+
+        //National Debt 2000-2017
+        //data saved to two decimals places
+        //https://fiscaldata.treasury.gov/datasets/debt-to-the-penny/debt-to-the-penny
+        d3.csv("data/DebtPenny_2000_2017.csv")
+        .then((data) => {
+            
+        })
+
+        // d3.csv("assets/beer.csv")
+        // .then((data) => {   
+        //     this.cartogram.render(data, year)
+        //     this.timeline.render(data, year);
+        //     this.bar.render(data, year);
+        // });
     }
 
     selectBrush(years) {
