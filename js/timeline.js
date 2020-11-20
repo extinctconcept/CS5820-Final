8da21f74ec8b8e7a139f07aead8a598f3869e463
Brandon Larsen <larsen.brand@outlook.com>
2020-11-19 23:38:50 -0700

    Adding more years

--- js/timeline.js
+++ js/timeline.js
@@ -44,7 +44,7 @@ class Timeline {
 
             this.svg.append("g").attr("class", "brush").call(brush);
     
-            let years = ["2001", "2002", "2003", "2004"];
+            let years = ["2000","2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"];
             let xscale = d3.scaleLinear()
                 .domain([0, years.length - 1])
                 .range([this.padding, this.width - this.padding]);
