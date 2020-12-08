da65b4bee31cbc380d1e75154e1dca29da723512
Brennon Netz <bnetz@gmail.com>
2020-12-07 18:33:13 -0700

    flights mostly working

--- js/flights.js
+++ js/flights.js
@@ -19,5 +19,65 @@ class Flights {
     svg.append("g").classed("yFlightAxis",true);
     svg.attr("height", svgHeight + margin.bottom);
     svg.attr("width", svgWidth);
+
+    let minYear = d3.min(data, (data) => data.Period);
+    let maxYear = d3.max(data, (data) => data.Period);
+    
+    let minStock = d3.min(data, (data) => data.Total);
+    let maxStock = d3.max(data, (data) => data.Total);
+
+    console.log(minStock, maxStock)
+    svg.select("#line")
+      .attr("width", svgWidth)
+      .attr("height", svgHeight)
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let dates = [];
+    data.forEach(function (d) {
+      let date = new Date(d.Period);
+      dates.push(date);
+    });
+
+    console.log(dates);
+
+    let xScale = d3.scaleTime().domain([dates[0], dates[dates.length-1]]).range([0, svgWidth]).nice();
+
+    let yScale = d3
+      .scaleLinear()
+      .domain([parseFloat(minStock), parseFloat(maxStock)])
+      .range([svgHeight - 40, 0]);
+
+    let yaxisWidth = 60;
+    const drawLine = d3
+      .line()
+      .x((d) => xScale(new Date(d.Period).valueOf()) + xScale(dates[1]) / 2)
+      .y((d) => yScale(d.Total));
+
+    svg
+      .selectAll("path")
+      .data(data)
+      .enter()
+      .append("path")
+      .attr("d", drawLine(data))
+      .attr("transform", `translate(${yaxisWidth},0)`)
+      .attr("stroke", "#105189")
+      .attr("stroke-width", 2)
+      .attr("fill", "transparent");
+
+    let xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%m/%Y"));
+      d3.select(".xFlightAxis")
+        .call(xAxis)
+        .attr("transform", `translate(${yaxisWidth}, ${svgHeight - 35})`)
+        .selectAll("text")
+        .attr("transform", "rotate(90)")
+        .attr("x", 9)
+        .attr("dy", "-.35em")
+        .style("text-anchor", "start");
+  
+      let yAxis = d3.axisLeft(yScale);
+      d3.select(".yFlightAxis")
+        .attr("transform", `translate(${yaxisWidth}, 0)`)
+        .call(yAxis)
+        .selectAll("text");
   }
 }
